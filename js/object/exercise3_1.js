a92c89e3d15f83435459acd3129dca4d5d7c9c7e
CzDgB <201801223@hufs.ac.kr>
2024-09-03 01:05:46 +0900

    update object_exercise3_1

--- js/object/exercise3_1.js
+++ js/object/exercise3_1.js
@@ -1,6 +1,14 @@
 // 원시값(primitive)만을 갖는 객체 kim을 복사하는 프로그램을 Object의 클래스 메소드 또는 spread(...) 연산자를  사용하지 말고 작성하시오.
 
 // shallow copy
+const copyObject = (obj) => {
+    const ret = {};
+    for (const key in obj){
+        ret[key] = obj[key];
+    }
+    return ret;
+}
+
 const kim = { nid: 3, nm: "Kim", addr: "Pusan" };
 const newKim = copyObject(kim);
 newKim.addr = "Daegu";
