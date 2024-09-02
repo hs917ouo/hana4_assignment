25b3c9bbfc9dba0403528d61479cae21f59d737f
CzDgB <201801223@hufs.ac.kr>
2024-09-03 00:35:20 +0900

    update object_exercise2

--- js/object/exercise2.js
+++ js/object/exercise2.js
@@ -1,3 +1,4 @@
+const assert = require("assert");
 // [['A', 10, 20], ['B', 30, 40], ['C', 50, 60, 70]] 배열을 객체로 만드시오.
 //(makeObjectFromArray)
 // => { 'A': [10, 20], 'B': [30, 40], 'C': [50, 60, 70] }
@@ -6,3 +7,26 @@
 // (makeArrayFromObject)
 // { 'A': [10, 20], 'B': [30, 40], 'C': [50, 60, 70] }
 // => [['A', 10, 20], ['B', 30, 40], ['C', 50, 60, 70]]
+
+const makeObjectFromArray = (arr) => {
+    const ret = {};
+    for (const idx in arr){
+        const [key,...values] = [...arr[idx]];
+        Object.assign(ret,{[key]:values});
+    }
+    return ret;
+}
+
+const makeArrayFromObject = (obj) => {
+    const ret = [];
+    for(const key in obj){
+        ret.push([key,...obj[key]]);
+    }
+    return ret;
+}
+
+// makeObjectFromArray([['A', 10, 20], ['B', 30, 40], ['C', 50, 60, 70]]);
+// makeArrayFromObject({ 'A': [10, 20], 'B': [30, 40], 'C': [50, 60, 70] });
+
+assert.deepStrictEqual(makeObjectFromArray([['A', 10, 20], ['B', 30, 40], ['C', 50, 60, 70]]),{ 'A': [10, 20], 'B': [30, 40], 'C': [50, 60, 70] });
+assert.deepStrictEqual(makeArrayFromObject({ 'A': [10, 20], 'B': [30, 40], 'C': [50, 60, 70] }),[['A', 10, 20], ['B', 30, 40], ['C', 50, 60, 70]]);
