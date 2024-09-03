24de03bcdaf4c652900b23d9f721aec57cfd6c01
campus2 H015 <h015@alpacoedu.onmicrosoft.com>
2024-09-03 17:35:03 +0900

    update fn_exercise1

--- js/fn/exercise1.js
+++ js/fn/exercise1.js
@@ -12,3 +12,16 @@
 // };
 
 // dog.whatsYourName();
+
+const dog = {
+  name: "Maxx",
+  showMyName() {
+    console.log(`My name is ${this.name}.`);
+  },
+  whatsYourName() {
+    setTimeout(this.showMyName.bind(this), 1000);
+    // setTimeout(() => this.showMyName(), 1000);
+  },
+};
+
+dog.whatsYourName();
