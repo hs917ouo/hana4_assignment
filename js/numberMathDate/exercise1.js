bebf9a1eadf4608a969e2e63b42c9909448339eb
campus2 H015 <h015@alpacoedu.onmicrosoft.com>
2024-09-03 17:30:48 +0900

    update numberMathDate_exercise1

--- js/numberMathDate/exercise1.js
+++ js/numberMathDate/exercise1.js
@@ -5,3 +5,36 @@
 // 3) 내년(2025년)의 오늘(9월 3일)의 요일을 출력하시오.
 
 // 4) 오늘(9월 3일)로 부터 100일 후의 날짜는?
+
+fn1 = () => {
+  date1 = new Date(1970, 1, 1);
+  date2 = new Date(1970, 1, 2);
+  console.log((date2 - date1) / 1000);
+};
+
+fn2 = () => {
+  date = new Date();
+  tmp_date = new Date(date.getFullYear(), date.getMonth(), 0);
+  console.log(tmp_date.getDate());
+  day = () => 1 + Math.round((tmp_date.getDate() - 1) * Math.random());
+  let arr = [];
+  for (i = 0; i < 5; i++) {
+    arr.push(day());
+  }
+
+  arr.sort((a, b) => b - a);
+  console.log(arr);
+};
+
+fn3 = () => {
+  date = new Date();
+  date.setFullYear(date.getFullYear() + 1);
+  const day = { 0: "토", 1: "일", 2: "월", 3: "화", 4: "수", 5: "목", 6: "금" };
+  console.log(`${day[date.getDay()]}요일`);
+};
+
+fn4 = () => {
+  date = new Date();
+  date.setDate(date.getDate() + 100);
+  console.log(date);
+};
